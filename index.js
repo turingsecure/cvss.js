8d64ee8b742c44e8d509a6fa916f14e062d360e1
Fabian Kutsche <kutschefabian@gmail.com>
2020-10-02 11:43:00 +0200

    remove console.log

--- index.js
+++ index.js
@@ -1,8 +1,3 @@
 const CVSS = require("./lib/cvss.js");
 
-const vector = CVSS("CVSS:3.0/AV:N/AC:H/PR:L/UI:R/S:C/C:N/I:L/A:L");
-
-console.log(vector.getScore());
-console.log(vector.vector);
-
 module.exports = CVSS;
