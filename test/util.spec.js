74ded0885e2aa8643befb2dfb979be0a103bf04e
miko37x <tim.kahmen@protonmail.com>
2020-10-13 17:34:27 +0200

    added the roundUpExact function #54

---
+++ test/util.spec.js
@@ -0,0 +1,15 @@
+const util = require("../lib/util");
+
+
+describe("roundUpExact Tests", () => {
+    it("Should return the Number", () => {
+
+        expect(util.roundUpExact(0.1+0.2)).toBe(0.3);
+
+        expect(util.roundUpExact(0.6+0.2)).toBe(0.8);
+
+        expect(util.roundUpExact(0.4+0.2)).toBe(0.6);
+
+        expect(util.roundUpExact(0.8+0.2)).toBe(1);
+    });
+  });
