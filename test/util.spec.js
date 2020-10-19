849f01215f22d3bf8cad4091b37285c4ff8e8a0f
miko37x <tim.kahmen@protonmail.com>
2020-10-19 13:08:43 +0200

    added the getCleanString function

--- test/util.spec.js
+++ test/util.spec.js
@@ -1,3 +1,4 @@
+const { getCleanVectorString } = require("../lib/util");
 const util = require("../lib/util");
 
 describe("roundUpExact Tests", () => {
@@ -9,5 +10,14 @@ describe("roundUpExact Tests", () => {
     expect(util.roundUpExact(0.4+0.2)).toBe(0.6);
 
     expect(util.roundUpExact(0.8+0.2)).toBe(1);
+  });
+});
+
+describe("getCleanVectorString Tests", () => {
+  it("Should return the String", () => {
+    const vectorObject = "CVSS:3.0/AV:L/AC:L/PR:H/UI:N/S:U/C:N/I:H/A:N/E:P/RL:W/RC:X/CR:X/IR:X/AR:M/MAV:A/MAC:X/MPR:X/MUI:N/MS:X/MC:X/MI:X/MA:X";
+
+    expect(getCleanVectorString(vectorObject)).toBe("CVSS:3.0/AV:L/AC:L/PR:H/UI:N/S:U/C:N/I:H/A:N/E:P/RL:W/AR:M/MAV:A/MUI:N");
+
   });
 });
